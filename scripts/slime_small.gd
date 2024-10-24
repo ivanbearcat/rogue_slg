4c86a835e9073f48a89c00cd54d1594afd639bcf
BearCat <ivanhope@163.com>
2024-10-24 23:30:18 +0800

    修改：史莱姆场景修改

---
+++ scripts/slime_small.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var enemy_grid_index: Vector2i
+var enemy_hp: int = 1
+var target_position: Vector2
+
+func _process(delta: float) -> void:
+	if target_position:
+		if self.position == target_position:
+			target_position = Vector2.ZERO
+		else:
+			self.position = self.position.move_toward(target_position, 15 * delta)
